36c264fca684e08efd4eed4fb9e0cf7dea5c5991
marimar <mariayuran@yandex.by>
2025-09-04 19:31:04 +0300

    vite jsx

---
+++ my-app-vite/src/App.jsx
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import { createElement } from "react";
+
+function App() {
+	return (
+		<div className="App">
+			{/*Декларативный*/}
+			<header className="App-header">
+				{/* Декларативный: описываем структуру и свойства компонента */}
+				<img src={logo} className="App-logo" alt="logo" />
+				{/* Декларативный: просто JSX-описание */}
+				<p>
+					Edit <code>src/App.jsx</code> and save to reload.
+				</p>
+				{/* Декларативный: ссылка с атрибутами */}
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				{/* Императивный элемент: явное указание КАК получить год, а не просто ЧТО отобразить.*/}
+				<span>{new Date().getFullYear()}</span>
+			</header>
+		</div>
+	);
+}
+
+export default App;
